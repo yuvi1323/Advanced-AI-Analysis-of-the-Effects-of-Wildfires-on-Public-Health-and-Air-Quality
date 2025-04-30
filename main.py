090c428aa841013d2a0c5e8755b5cbe71ae07959
Yuva Raj Jami <61086975+yuvi1323@users.noreply.github.com>
2025-04-30 17:41:34 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from utils.dataset_builder import load_dataset
-
-# Load and print one batch
-dataset = load_dataset("data/wildfire_tf_data")
-
-print("TFRecord dataset loaded successfully! Sample batch shapes:")
-for batch in dataset.take(1):
-    for key, value in batch.items():
-        print(f"{key}: shape = {value.shape}")
